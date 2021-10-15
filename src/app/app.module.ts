4563de8ed4ff661efb77f1fb8d40f29666fffdc6
Marcian Bondoc <marcian.bondoc@gmail.com>
2021-10-15 22:45:30 +0300

    solved bugs, fixed push notification and pwa issues

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, APP_INITIALIZER } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { PwaService } from 'src/app/shared/services/pwa.service';
+import { MatBottomSheet } from '@angular/material/bottom-sheet';
 import { ToastrModule } from 'ngx-toastr';
 import { FormsModule } from '@angular/forms';
 
+const initializer = (pwaService: PwaService) => () => pwaService.initPwaPrompt();
+ 
 @NgModule({
   declarations: [
     AppComponent
@@ -22,7 +26,9 @@ import { FormsModule } from '@angular/forms';
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
     ToastrModule.forRoot()
   ],
-  providers: [],
+  providers: [
+    {provide: MatBottomSheet, useValue: {} },
+    {provide: APP_INITIALIZER, useFactory: initializer, deps: [PwaService], multi: true},],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
